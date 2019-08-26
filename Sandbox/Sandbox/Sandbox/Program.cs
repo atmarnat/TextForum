821e9c87536c9044d9394c5057671f24ec3571bc
atmar <nathaniel.atmar@outlook.com>
2019-08-26 10:02:31 -0700

    Did much more cleanup, and created links between pages

--- Sandbox/Sandbox/Sandbox/Program.cs
+++ Sandbox/Sandbox/Sandbox/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Sandbox
+namespace MssaMvcApp
 {
     public class Program
     {
