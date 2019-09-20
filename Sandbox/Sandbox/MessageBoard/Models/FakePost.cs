4c5cacdd629ddcf592612a14431564956644387d
atmar <nathaniel.atmar@outlook.com>
2019-09-20 14:34:07 -0700

    Created an instance of fake data

---
+++ Sandbox/Sandbox/MessageBoard/Models/FakePost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoard.Models
+{
+    public class FakePost
+    {
+        //This just sets values so I don't have to worry about being on a database
+        public int PostID { get; set; } = 0;
+        public int TopicID { get; set; } = 1;
+        public int UserID { get; set; } = 1;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public string Content { get; set; } = "Sample message";
+    }
+}
