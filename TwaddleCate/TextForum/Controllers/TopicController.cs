aac775fc13dcdae27a7b0a558263a0ad8e127d8a
atmar <nathaniel.atmar@outlook.com>
2019-10-07 15:47:18 -0700

    Switched file naming system
    went from MD5 encryption on the datetime to GUID

--- TwaddleCate/TextForum/Controllers/TopicController.cs
+++ TwaddleCate/TextForum/Controllers/TopicController.cs
@@ -108,7 +108,7 @@ namespace TextForum.Controllers
             {
                 MD5 md5Hash = MD5.Create();
                 string imgPath = "wwwroot/Images/";
-                string imgName = GetMd5Hash(md5Hash, newPost.Created + DateTime.UtcNow.ToString());
+                string imgName = Guid.NewGuid().ToString();
                 string extension = "";
                 for (int i = file[0].FileName.Length - 1; i > 0; i--)
                 {
@@ -148,26 +148,6 @@ namespace TextForum.Controllers
         public IActionResult NavMenu()
         {
             return RedirectToAction("List");
-        }
-        static string GetMd5Hash(MD5 md5Hash, string input)
-        {
-
-            // Convert the input string to a byte array and compute the hash.
-            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
-            StringBuilder sBuilder = new StringBuilder();
-
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
-            for (int i = 0; i < data.Length; i++)
-            {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-
-            // Return the hexadecimal string.
-            return sBuilder.ToString();
         }
     }
 }
